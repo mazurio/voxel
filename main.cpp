c1d86a5f0cd85f8b33833d0102c6ac10b5ae07c8
Damian Mazurkiewicz <Damian.Mazurkiewicz@bbc.co.uk>
2015-04-11 15:13:00 +0100

    Added OpenGL linking.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,26 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+
+using namespace sf;
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-
-    shape.setFillColor(sf::Color::Green);
+    RenderWindow window(VideoMode(800, 600), "Voxel Engine", Style::Default, ContextSettings(32));
 
     while(window.isOpen()) {
-        sf::Event event;
+        Event event;
 
         while(window.pollEvent(event)) {
-            if(event.type == sf::Event::Closed) {
+            if(event.type == Event::Closed) {
                 window.close();
+            } else if(event.type == Event::Resized) {
+                // Adjust the viewport when window is resized.
+                glViewport(0, 0, event.size.width, event.size.height);
             }
         }
 
-        window.clear();
-        window.draw(shape);
+        // Clear the buffers.
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         window.display();
     }
 
